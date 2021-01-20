75ff71d54a9774f088e7ac1d91210423426fa666
JunYeob-Do <dydrltk1919@naver.com>
2021-01-20 16:17:25 +0900

    크롤러 어떻게 할지 구상중 requirements.txt제작

---
+++ crawler.py
@@ -0,0 +1,8 @@
+#네이버에 검색해서 단어들어있으면 제목/링크저장.
+
+#네이버 뉴스는 greenbox 클래스에 value에 값을 넣는 것으로 검색한다.
+#키워드 = 스마트 팩토리 검색. 최신순 정렬시 url.
+#https://search.naver.com/search.naver?
+# where=news&query=%EC%8A%A4%EB%A7%88%ED%8A%B8%20%ED%8C%A9%ED%86%A0%EB%A6%AC
+# &sm=tab_srt&sort=1&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid=&nso=so%3Add%2Cp%3Aall%2Ca%3Aall
+# &mynews=0&refresh_start=0&related=0
